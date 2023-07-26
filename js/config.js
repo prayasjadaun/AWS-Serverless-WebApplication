add038334d6fe27e1e7a4ef0ce2233caca402f98
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2023-07-26 22:34:05 +0530

    'upload'

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_ABOA8q61S', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2ikqcnj16jmak9pca0hdsh2chj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://oj9lh7xva1.execute-api.ap-south-1.amazonaws.com/production' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
